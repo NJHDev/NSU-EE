2f1a689b108cf0d01bea5470408549cd156d2857
NJHDev <mark0112@naver.com>
2025-03-20 13:32:51 +0900

    Create 3week.c

---
+++ 3-1/마이크로프로세서실습/C언어/3week.c
@@ -0,0 +1,24 @@
+/* 3-1 사칙연산자 계산
+#include <stdio.h>
+
+int main() {
+    int input = 100, input2 = 5, sum = 0, sub = 0, multi = 0, divid = 0, mod = 0;
+
+    sum = input + input2;
+    sub = input - input2;
+    multi = input * input2;
+    divid = input / input2;
+    mod = input % input2;
+
+    printf("덧셈결과 = %d\n", sum);
+    printf("뺄셈결과 = %d\n", sub);
+    printf("곱셈결과 = %d\n", multi);
+    printf("나눗셈결과 = %d\n", divid);
+    printf("모듈로결과 = %d\n", mod);
+    
+    return 0;
+    
+}
+*/
+
+/* 3-2 비교연산자 계산
