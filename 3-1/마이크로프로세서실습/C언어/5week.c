73efd41e5cb9fe3c9d3fe5277909006b2bf8e621
NJHDev <mark0112@naver.com>
2025-04-03 14:11:42 +0900

    Update 5week.c

--- 3-1/마이크로프로세서실습/C언어/5week.c
+++ 3-1/마이크로프로세서실습/C언어/5week.c
@@ -160,3 +160,39 @@
         printf("%d까지의 합: %d\n", n, sum);
         return 0;
     }
+
+/*문제7*/
+/*구구단 출력프로그램*/
+    /*세로*/
+    #include <stdio.h>
+    
+    int main() {
+        int i;
+        int j;
+    
+        for(j=1; j<=9; j++) {
+          for(i = 1; i <= 9; i++) {
+            printf("%d x %d = %d\n", j, i, j*i);
+          }
+          printf("\n");
+       }
+      
+        return 0;
+    }
+
+    /*가로*/
+    #include <stdio.h>
+    
+    int main() {
+        int i;
+        int j;
+        
+        for(i = 1; i <= 9; i++) {
+            for(j = 1; j <= 9; j++) {
+                printf("%d x %d = %d   ", j, i, j * i);
+            }
+            printf("\n");
+        }
+        
+        return 0;
+    }
