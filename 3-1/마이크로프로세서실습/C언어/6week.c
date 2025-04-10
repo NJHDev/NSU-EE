6d60ca56dac605bfc7bdc49ff89cfaacc0d17c0b
NJHDev <mark0112@naver.com>
2025-04-10 13:46:08 +0900

    Update 6week.c

--- 3-1/마이크로프로세서실습/C언어/6week.c
+++ 3-1/마이크로프로세서실습/C언어/6week.c
@@ -52,6 +52,21 @@
         return 0;
     }
 
+/*문제5*/
+    #include <stdio.h>
+    
+    int main() {
+      int i = 3;
+    
+      while (i) {
+        printf("%d은 참입니다.\n", i);
+        i--;
+      }
+    
+      printf("%d은 거짓입니다.\n", i);
+    
+      return 0;
+    }
 
 /*문제6*/
 /*1부터n까지의 합*/
